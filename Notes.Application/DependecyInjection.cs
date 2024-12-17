111d7fb947bcd6bc61603bc3bb3afabdab6816f1
ripper <rippergods@gmail.com>
2024-12-17 19:48:24 +0300

    Добавил зависимости

---
+++ Notes.Application/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Application;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        return services;
+    }
+}
